15b2348daa3a1bbdf940ef345ea753464a6c1855
Micu Matei-Marius <micumatei@gmail.com>
2017-02-11 05:50:44 +0200

    Added a pipeline factory

---
+++ arias/pipelines/factory.py
@@ -0,0 +1,21 @@
+"""Factory for all the pipelines."""
+
+from arias.common import util
+from arias.pipelines import base
+
+class PipelineFactory(util.BaseFactory):
+
+    # The base class for all the items
+    BASE_CLASS = base.BasePipeline
+
+    # The prefix patch for all the items
+    PREFIX = "arias.pipelines"
+
+    # A list with all the items
+    ITEMS = [
+        "dmoz_pipeline",
+        "redis_pipeline"
+    ]
+
+    # The name of the factory
+    NAME = "pipeline_factory"
