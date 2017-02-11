bf91d1b6718f4a759601029b7fb5da817020448c
Micu Matei-Marius <micumatei@gmail.com>
2017-02-11 15:15:05 +0200

    Added task factory

---
+++ arias/tasks/factory.py
@@ -0,0 +1,40 @@
+"""Factory for tasks."""
+
+from arias.tasks import task
+from arias.spiders import factory as spider_factory
+from arias.pipelines import factory as pipeline_factory
+from arias.common import exception
+
+def get_task(spider_name, pipeline_name):
+    """Get a new Task object.
+    
+    :param spider_name: The spider name you want to use.
+    :param pipeline_name: The pipeline name you want to use
+    """
+    spider = None
+    pipeline = None
+
+    # Get the spider class
+    for item in spider_factory.SpiderFactory.get_items():
+        if item.name == spider_name:
+            spider = item
+            break
+
+    # Get the pipeline class
+    for item in pipeline_factory.PipelineFactory.get_items():
+        if item.__name__ == pipeline_name:
+            pipeline = item
+            break
+
+    if spider is None:
+        raise exception.InvalidName(
+            name=spider_name, list_name="spiders")
+
+    if pipeline is None:
+        raise exception.InvalidName(
+            name=pipeline_name, list_name="pipelines")
+
+    return task.AriasTasks(spider=spider, pipeline=pipeline)
+
+
+
