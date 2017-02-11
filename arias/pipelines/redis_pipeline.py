d44f4980d2abaeda29bed7b945d274e0f21f1230
Micu Matei-Marius <micumatei@gmail.com>
2017-02-11 14:35:45 +0200

    Eliminate unused import

--- arias/pipelines/redis_pipeline.py
+++ arias/pipelines/redis_pipeline.py
@@ -4,16 +4,12 @@ import json
 
 from arias.common import redisdb
 from arias.pipelines import base
-from arias.items import car
 
-class CarPipeline(base.BasePipeline):
+class RedisPipeline(base.BasePipeline):
 
     def __init__(self):
         self._rc = redisdb.RedisConnection()
 
-
     def process_item(self, item, spider):
-        self._redis.rcon.hset(self._spider.website, str(key), 
+        self._redis.rcon.hset(self._spider.name, str(key), 
                               json.dumps(dict(item)))
-
-
