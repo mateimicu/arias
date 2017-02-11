e5c9f8f38cfc5b731324b4720e7804ccb0f37c96
Micu Matei-Marius <micumatei@gmail.com>
2017-02-11 05:39:35 +0200

    Added helper function and base class for factories

--- arias/common/util.py
+++ arias/common/util.py
@@ -1,7 +1,7 @@
 """A collection of utilities used across the project."""
 
 from arias.common import exception
-
+import importlib
 
 def get_attribute(root, attribute):
     """Search for the received attribute name in the object tree.
@@ -22,3 +22,46 @@ def get_attribute(root, attribute):
     raise exception.AriasException("The %(attribute)r attribute is "
                                    "missing from the object tree.",
                                    attribute=attribute)
+
+def join_with_space(items):
+    """Join with `, ` the items.
+
+    :param items: The items we want to join
+    """
+    return ", ".join([str(item) for item in items])
+
+
+class BaseFactory(object):
+    """Base class for all Factories."""
+
+    # The base class for all the items
+    BASE_CLASS = None
+
+    # The prefix patch for all the items
+    PREFIX = ""
+
+    # A list with all the items
+    ITEMS = []
+
+    # The name of the factory
+    NAME = ""
+
+
+    @classmethod
+    def get_items(cls):
+        """Return a list with all the items."""
+        all_items= []
+        for item_module in cls.ITEMS:
+            module_name = "{}.{}".format(cls.PREFIX, item_module)
+            module = importlib.import_module(module_name)
+            for item in dir(module):
+                item = getattr(module, item)
+                try:
+                    if (not issubclass(item, cls.BASE_CLASS) or
+                    item == cls.BASE_CLASS):
+                        continue
+                except (exception.AriasException, TypeError):
+                    continue
+                all_items.append(item)
+
+        return all_items
